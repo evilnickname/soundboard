dcd1eb965a6e77a57cbec2dd9b6a022ad5244c55
William Groenendijk <william.groenendijk@gmail.com>
2014-07-15 17:02:02 +0200

    Nearing version 1.0

--- soundboard.js
+++ soundboard.js
@@ -9,95 +9,145 @@ var soundboard = {
 		request.open('GET', 'soundboards.json', true);
 
 		request.onload = function () {
-			if (request.status >= 200 && request.status < 400) {
-				// Success!
+			if (request.status >= 200 && request.status < 400) { // Success!
                 soundboard.data = JSON.parse(request.responseText);
 				soundboard.setup.firstRun();
-			} else {
-				// We reached our target server, but it returned an error
+			} else { // We reached our target server, but it returned an error
+				soundboard.render.html('soundstage', '<p class="error">Something went wrong on our end. Please try again.</p>');
 			}
 		};
 
-		request.onerror = function () {
-		  // There was a connection error of some sort
-		};
+		request.onerror = function () { // There was a connection error of some sort
+			soundboard.render.html('soundstage', '<p class="error">Server says no.</p>');
+		};			
 
 		request.send();
 	},
     
-    cleanup : {
-        board : function (id) {
-            var elem = document.getElementById(id);
-            while (elem.firstChild) {
-                elem.removeChild(elem.firstChild);
-            }        
-        }
+    cleanup :  function (id) {
+		var elem = document.getElementById(id);
+		while (elem.firstChild) {
+			elem.removeChild(elem.firstChild);
+		}
     },
 
     render : {
         board : function () {
-            var sounds = soundboard.data.boards[soundboard.currentBoardId].sounds,
+            var active = soundboard.currentBoardId;
+			soundboard.cleanup('soundstage');
+            document.getElementById('soundstage').appendChild(soundboard.render.buttons());
+            soundboard.render.string(
+				'title', soundboard.data.boards[soundboard.currentBoardId].name
+			);
+			soundboard.render.html(
+				'smallprint', soundboard.data.boards[soundboard.currentBoardId].disclaimer
+			);
+			soundboard.render.html(
+				'description', soundboard.data.boards[soundboard.currentBoardId].description
+			);
+			document.querySelectorAll('#choosesoundboard button')[active].classList.add('active');
+		},
+		buttons : function () {
+			var	sounds = soundboard.data.boards[soundboard.currentBoardId].sounds,
                 btns = document.createDocumentFragment();
             for (var i = 0, n = sounds.length; i < n; i++) {
-                soundboard.sprite[sounds[i].tag] = sounds[i].timing;
                 btns.appendChild(soundboard.render.button(sounds[i]));       
-            }
-            soundboard.cleanup.board('board');
-            document.getElementById('board').appendChild(btns);
-            document.getElementById('title').textContent = soundboard.data.boards[soundboard.currentBoardId].name;
-            soundboard.setup.howler();
-            soundboard.setup.buttons();
-            soundboard.setup.switch();
-        },
-        
+            }	
+			return btns;
+		},
         button : function (data) {
             var b = document.createElement('BUTTON');
             b.setAttribute('data-sound', data.tag);
+            b.setAttribute('type', 'button');
+            b.setAttribute('class', 'soundboard__button');
+			b.addEventListener('click', function() {
+				soundboard.sound.play(this.getAttribute('data-sound'));
+			});
             b.appendChild(document.createTextNode(data.text));
             return b;
-        }
-    },
-        
-    setup : {
-        buttons : function () {
-            var btns = document.querySelectorAll('button');
-            for (var i=0, n = btns.length; i < n; i++) {
-                btns[i].addEventListener('click', function() {
-                    soundboard.sound.play(this.getAttribute('data-sound'));
-                });
-            }
         },
-
-        chooser : function () {
+		html: function (elemId, html) {
+			if (typeof html !== 'undefined') {
+				document.getElementById(elemId).innerHTML = html;
+			} else {
+				soundboard.cleanup(elemId)
+			}
+        },
+		string: function (elemId, string) {
+			if (typeof string !== 'undefined') {
+				document.getElementById(elemId).textContent = string;
+			} else {
+				soundboard.cleanup(elemId)
+			}
+		}
+    },
+	
+    setup : {
+		chooser : function () {
             var b = soundboard.data.boards,
                 btns = document.createDocumentFragment();
 
-            for (var i = 0, n = b.length; i < n; i++) {
-                var opt = document.createElement('OPTION');
-                opt.setAttribute('value', i);
-                opt.appendChild(document.createTextNode(b[i].name));
-                document.getElementById('choosesoundboard').appendChild(opt);
-            }    
+			if (b.length > 1) {
+				for (var i = 0, n = b.length; i < n; i++) {
+					var button = document.createElement('BUTTON');
+					button.setAttribute('value', i);
+					button.setAttribute('type', 'button');
+					button.setAttribute('class', 'sidebar__button');
+					button.appendChild(document.createTextNode(b[i].name));
+					button.addEventListener('click', function() {
+						if (this.classList.contains('active')) { return };
+						soundboard.currentBoardId = this.value;
+						document.querySelector('#choosesoundboard .active').classList.remove('active');
+						soundboard.render.html('soundstage', '<p class="loading">fetching sounds</p>');
+						soundboard.setup.howler();
+					});
+					btns.appendChild(button);
+				}
+				document.getElementById('choosesoundboard').appendChild(btns);
+			} else {
+				document.documentElement.classList.add('sidebarNotActive');
+			}
         },
-
+		
         firstRun : function () {
             soundboard.setup.chooser();
-            soundboard.render.board();
+			soundboard.setup.howler();
         },
         
         howler : function () {
             soundboard.sound = new Howl({
                 urls: soundboard.data.boards[soundboard.currentBoardId].files,
-                sprite: soundboard.sprite
+                sprite: soundboard.setup.sprite(),
+				onload: function () {
+					//console.log('"' + soundboard.sound._src + '" loaded.');
+					soundboard.render.board();
+				},
+				onloaderror: function (e) {
+					//console.log('"' + soundboard.sound._src + '" failed to load.');
+					soundboard.setup.tryNextFile(soundboard.sound._src);
+				}
             })
         },
-    
-        switch : function () {
-            document.querySelector('select').addEventListener('change', function() {
-                soundboard.currentBoardId = this.value;
-                soundboard.render.board();
-            });
-        }
+		
+		sprite : function () {
+			var	sounds = soundboard.data.boards[soundboard.currentBoardId].sounds,
+				sprite = {};
+            for (var i = 0, n = sounds.length; i < n; i++) {
+                sprite[sounds[i].tag] = sounds[i].timing;    
+            } 
+			return sprite;
+		},
+
+		tryNextFile : function (nothisone) {
+			var urls = soundboard.data.boards[soundboard.currentBoardId].files,
+				notme = urls.indexOf(nothisone);
+			soundboard.sound.urls = urls.splice(notme, 1);
+			if (soundboard.sound.urls > 0) {
+				soundboard.render.board();
+			} else {
+				soundboard.render.html('soundstage', '<p class="error">No sound could be loaded.</p>');
+			}
+		}
     }
 };
 
